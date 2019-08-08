4f081f4e1b579424ce81ba8cc04999ba890db373
Handmade Studios <alexhawkingutube@gmail.com>
2019-08-08 10:27:11 +0800

    make pip ready

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'coolors',
+  packages = ['coolors'],
+  version = '0.1',
+  license='MIT',
+  description = 'A Python module to make text colouring easy.',
+  author = 'Alex Hawking',
+  author_email = 'alexrhawking@gmail.com',
+  url = 'https://alexhawking.now.sh/',
+  download_url = 'https://github.com/Handmade-Studios/coolors-module/archive/master.zip',
+  keywords = ['colour', 'easy', 'output', 'terminal', 'colouring', 'color', 'cool'],
+  install_requires=[
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
